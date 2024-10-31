ccd2c8967aa4bb42ff22449017285f369934fbd7
Wektorous <136726144+Wektorous@users.noreply.github.com>
2024-10-31 16:36:47 +0100

    rezv06-hw06

---
+++ www/rezv06/hw06/js/main.js
@@ -0,0 +1,74 @@
+/**
+ * Long live Sparta! Vytvořte funkci, která vyřeší Caesarovu širfu. Funkce dostane 
+ * na vstup zašifrovaný text a také hodnotu, která byla použita při šifrování, a pak 
+ * vrátí dešifrovaný text. Předpokládejte pouze anglickou abecedu s velkými 
+ * písmeny, ostatní znaky ignorujte. Poté v konzoli dešifrujte/dešiftujte následující texty.
+ * 
+ * key used - encrypted text
+ *       19 - MPH MABGZL TKX BGYBGBMX: MAX NGBOXKLX TGW ANFTG LMNIBWBMR; TGW B'F GHM LNKX TUHNM MAX NGBOXKLX. - TEUXKM XBGLMXBG
+ *        5 - YMJWJ NX ST QFB JCHJUY YMJ QFB YMFY YMJWJ NX ST QFB. - OTMS FWHMNGFQI BMJJQJW
+ *       12 - M YMZ ITA PMDQE FA IMEFQ AZQ TAGD AR FUYQ TME ZAF PUEOAHQDQP FTQ HMXGQ AR XURQ. ― OTMDXQE PMDIUZ
+ * 
+ * Následně vytvořte uživatelské rozhraní, ve kterém bude možné zadat zmíněné dvě 
+ * vstupní hodnoty (zašifrovaný text a použitý klíč) a po kliknutí na tlačítko 
+ * "Decipher!" se na určeném místě zobrazí dešifrovaný text. Rozhraní také vhodně
+ * nastylujte.
+ */
+//         index: 0123456789...
+const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+const shiftChar = (c, shift) => {
+    // a helper function to shift one character inside the 
+    // alphabet based on the shift value and return the result
+
+    const index = alphabet.indexOf(c);
+
+    if (index === -1) return c;
+
+    return alphabet[(index - shift + 26) % 26]
+
+};
+
+
+const shiftString = (str, shift) => {
+    // a helper function to shift one entire string inside the 
+    // alphabet based on the shift value and return the result
+
+    return str.split('').map(c => shiftChar(c, shift)).join('');
+};
+
+
+const caesarDecipher = (cipherText, usedKey) => {
+
+    return shiftString(cipherText.toUpperCase(), usedKey);
+
+
+    // your implementation goes here
+    // good to know: 
+    //    str.indexOf(c) - returns the index of the specified character in the string
+    //    str.charAt(i) - returns the character at the specified index in the string
+    //    when the shifted character is out of bound, it goes back to the beginning and count on from there
+};
+
+// albert einstein
+caesarDecipher("MPH MABGZL TKX BGYBGBMX: MAX NGBOXKLX TGW ANFTG LMNIBWBMR; TGW B'F GHM LNKX TUHNM MAX NGBOXKLX. - TEUXKM XBGLMXBG", 19);
+
+
+// john archibald wheeler
+caesarDecipher("YMJWJ NX ST QFB JCHJUY YMJ QFB YMFY YMJWJ NX ST QFB. - OTMS FWHMNGFQI BMJJQJW", 5);
+
+// charles darwin
+caesarDecipher("M YMZ ITA PMDQE FA IMEFQ AZQ TAGD AR FUYQ TME ZAF PUEOAHQDQP FTQ HMXGQ AR XURQ. â€• OTMDXQE PMDIUZ", 12);
+
+const form = document.querySelector('form');
+const results = document.querySelector('#results');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const formData = new FormData(form);
+    const { text, key} = Object.fromEntries(formData);    
+    const decipheredText = caesarDecipher(text, key);
+
+    results.textContent = decipheredText;
+});
+
