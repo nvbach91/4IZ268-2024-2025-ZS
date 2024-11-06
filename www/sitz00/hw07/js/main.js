b4bb23948921de89b9ed2666af3132c45e15a07f
ZuzSi <zuzana.sitancova@gmail.com>
2024-11-06 22:29:32 +0100

    sitz00-hw07
    odevzdávám úkol č.7 [https://eso.vse.cz/~sitz00/cv07/](https://eso.vse.cz/~sitz00/cv07/)
    Reviewer: @cmda9 prosím :)

---
+++ www/sitz00/hw07/js/main.js
@@ -0,0 +1,89 @@
+/**
+ * Memorama, Match Match, Match Up, Memory, Pelmanism, Shinkei-suijaku, Pexeso or simply Pairs
+ */
+
+let cities = ['Prague', 'Tokyo', 'Sydney', 'Toronto', 'Bangkok', 'Singapore',  'Dubai', 'Mexico City', 'Buenos Aires', 'Hong Kong'];
+cities = cities.concat(cities);
+cities.sort(() => {
+  return 0.5 - Math.random();
+});
+
+const pexesoCards = document.getElementById('pexeso-cards');
+const pointsDisplay = document.getElementById('points');
+    let points = 0;
+
+const newCard = (city) => {
+    const card = document.createElement('div');
+    card.classList.add('card');
+    card.innerText = '';
+    card.addEventListener('click', () => {
+        openCard(card, city); 
+    });
+    return card;
+};
+
+if (pexesoCards) 
+    cities.forEach(city => {
+    const card = newCard(city);
+    pexesoCards.appendChild(card);
+});
+
+let firstCard = null;
+let secondCard = null;
+let lockBoard = false;
+
+const openCard = (card, city) => {
+    if (lockBoard) return;
+    if (card === firstCard || card.classList.contains('revealed')) return;
+
+    card.classList.add('revealed');
+    card.innerText = city;
+
+    if (firstCard === null) {
+        firstCard = card;
+    } else {
+        secondCard = card;
+        lockBoard = true;
+        compareCards();
+    }
+};
+
+const compareCards = () => {
+    if (firstCard.innerText === secondCard.innerText) {
+        points++;
+        pointsDisplay.innerText = points; 
+        resetCards();
+        checkForWin();
+    } else {
+        points--;
+        if (points < 0) points = 0;
+        pointsDisplay.innerText = points; 
+        setTimeout(() => {
+            resetCards(); 
+        }, 1000);
+    }
+};
+
+const resetCards = () => {
+    if (firstCard && secondCard) {
+        if (firstCard.innerText !== secondCard.innerText) {
+            firstCard.classList.remove('revealed');
+            firstCard.innerText = '';
+            secondCard.classList.remove('revealed');
+            secondCard.innerText = '';
+        }
+    }
+
+    firstCard = null;
+    secondCard = null;
+    lockBoard = false;
+};
+
+const checkForWin = () => {
+    const allCards = document.querySelectorAll('.card');
+    const allRevealed = Array.from(allCards).every(card => card.classList.contains('revealed'));
+
+    if (allRevealed) {
+        alert("Congratulation! Points: " + points);
+    };
+};
