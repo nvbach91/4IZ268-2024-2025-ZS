8530eea6c9811ad46cbf7510d29d8ed59649716c
Nguyen Viet Bach <nvbach91@outlook.com>
2024-12-13 15:47:25 +0100

    fix indent

--- www/nguv03/cv13b/js/main.js
+++ www/nguv03/cv13b/js/main.js
@@ -36,59 +36,59 @@ const BASE_POSTER_URL = 'https://image.tmdb.org/t/p/original';
 
 const searchResultsContainer = document.querySelector('#search-results');
 const fetchMovieSearchResults = async (searchValue) => {
-    const url = `${BASE_API_URL}/3/search/movie?api_key=${API_KEY}&query=${searchValue}`;
-    const res = await fetch(url);
-    const data = await res.json();
-    return data;
+  const url = `${BASE_API_URL}/3/search/movie?api_key=${API_KEY}&query=${searchValue}`;
+  const res = await fetch(url);
+  const data = await res.json();
+  return data;
 };
 
 const renderMovieSearchResults = (data) => {
-    const { results } = data;
-    const movies = results.map((result) => {
-        const html = `
-            <div class="movie card col-4 text-center">
-                <img src="${BASE_POSTER_URL}${result.poster_path}" class="card-img-top" alt="${result.title}">
-                <div class="card-body">
-                    <h5 class="card-title">${result.title}</h5>
-                    <button
-                        class="btn btn-primary"
-                        data-bs-toggle="modal"
-                        data-bs-target="#movie-details-modal"
-                    >
-                        Details
-                    </button>
-                </div>
-            </div>
-        `;
-        const movieContainer = document.createElement('div');
-        movieContainer.innerHTML = html;
-        movieContainer.querySelector('button').addEventListener('click', async () => {
-            modalContainer.querySelector('.modal-body').innerHTML = `
-                <div class="spinner-border text-primary" role="status">
-                    <span class="visually-hidden">Loading...</span>
-                </div>
-            `;
-            const movieDetailsData = await fetchMovieDetails(result.id);
-            renderMovieDetails(movieDetailsData);
-        });
-        return movieContainer.firstElementChild;
+  const { results } = data;
+  const movies = results.map((result) => {
+    const html = `
+      <div class="movie card col-4 text-center">
+        <img src="${BASE_POSTER_URL}${result.poster_path}" class="card-img-top" alt="${result.title}">
+        <div class="card-body">
+          <h5 class="card-title">${result.title}</h5>
+          <button
+              class="btn btn-primary"
+              data-bs-toggle="modal"
+              data-bs-target="#movie-details-modal"
+          >
+            Details
+          </button>
+        </div>
+      </div>
+    `;
+    const movieContainer = document.createElement('div');
+    movieContainer.innerHTML = html;
+    movieContainer.querySelector('button').addEventListener('click', async () => {
+      modalContainer.querySelector('.modal-body').innerHTML = `
+          <div class="spinner-border text-primary" role="status">
+              <span class="visually-hidden">Loading...</span>
+          </div>
+      `;
+      const movieDetailsData = await fetchMovieDetails(result.id);
+      renderMovieDetails(movieDetailsData);
     });
-    searchResultsContainer.innerHTML = '';
-    searchResultsContainer.append(...movies);
+    return movieContainer.firstElementChild;
+  });
+  searchResultsContainer.innerHTML = '';
+  searchResultsContainer.append(...movies);
 };
 const movieDetailsContainer = document.querySelector('#movie-details');
 
 const fetchMovieDetails = async (movieId) => {
-    const url = `${BASE_API_URL}/3/movie/${movieId}?api_key=${API_KEY}`;
-    const res = await fetch(url);
-    const data = await res.json();
-    return data;
+  const url = `${BASE_API_URL}/3/movie/${movieId}?api_key=${API_KEY}`;
+  const res = await fetch(url);
+  const data = await res.json();
+  return data;
 };
 const modalContainer = document.querySelector('#movie-details-modal');
 const renderMovieDetails = (data) => {
-    // render elements
-    const { title, poster_path, release_date, genres, budget, overview } = data;
-    const dom = `
+  // render elements
+  const { title, poster_path, release_date, genres, budget, overview } = data;
+  const dom = `
         <div class="movie">
             <h3 class="movie-title">${title}</h3>
             <div class="movie-poster">
@@ -100,14 +100,14 @@ const renderMovieDetails = (data) => {
             <div class="movie-budget">$${budget}</div>
         </div>
     `;
-    modalContainer.querySelector('.modal-body').innerHTML = dom;
+  modalContainer.querySelector('.modal-body').innerHTML = dom;
 };
 
 const searchForm = document.querySelector('#search-form');
 searchForm.addEventListener('submit', async (e) => {
-    e.preventDefault();
-    const formData = new FormData(searchForm);
-    const { searchValue } = Object.fromEntries(formData);
-    const movieSearchData = await fetchMovieSearchResults(searchValue);
-    renderMovieSearchResults(movieSearchData);
+  e.preventDefault();
+  const formData = new FormData(searchForm);
+  const { searchValue } = Object.fromEntries(formData);
+  const movieSearchData = await fetchMovieSearchResults(searchValue);
+  renderMovieSearchResults(movieSearchData);
 });
