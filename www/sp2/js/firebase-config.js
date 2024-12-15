dcd442b688f962e090da4bdf991bea8faaae27e8
pilipasit <128810157+pilipasit@users.noreply.github.com>
2024-12-15 18:36:36 +0100

    Update firebase-config.js

--- www/sp2/js/firebase-config.js
+++ www/sp2/js/firebase-config.js
@@ -5,13 +5,13 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/9.16.0/firebase
 
 // Your Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAt3CcoMbVHj_Mr3-LWlfmzQCSrQ6gSb4Q",
-  authDomain: "pilo00-sp2-tabletopplanner.firebaseapp.com",
-  projectId: "pilo00-sp2-tabletopplanner",
-  storageBucket: "pilo00-sp2-tabletopplanner.firebasestorage.app",
-  messagingSenderId: "591379457476",
-  appId: "1:591379457476:web:9b47f931e0f31a46f2f091",
-  measurementId: "G-G6XPRZL5MM",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 };
 
 // Initialize Firebase
