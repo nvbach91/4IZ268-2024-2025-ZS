774ccda0ed6dfee952bba88787bb8f9ef6346e46
David Brablc <brad14@vse.cz>
2024-12-15 17:45:06 +0100

    initial version

---
+++ www/brad14/sp2/js/main.js
@@ -0,0 +1,27 @@
+const API_KEY = '9202d14af4a6eb24073e146f205c50d6';
+const BASE_URL_DATA = 'https://api.openweathermap.org/data/3.0/onecall?';
+const BASE_URL_GEO = 'https://api.openweathermap.org/geo/1.0/direct?';
+
+const constructUrl = (baseUrl, parameters) => {
+    const url = `${baseUrl}${parameters}&appid=${API_KEY}`;
+    return url;
+}
+
+const fetchWeatherData = async (lat, lon) => {
+    const url = constructUrl(BASE_URL_DATA, `lat=${lat}&lon=${lon}&units=metric`);
+    const reponse = await fetch(url);
+    const data = await reponse.json();
+    console.log(data);
+    return data;
+}
+
+const fetchCoordinates = async (city) => {
+    const url = constructUrl(BASE_URL_GEO, `q=${city}&limit=1`);
+    const reponse = await fetch(url);
+    const data = await reponse.json();
+    console.log(data);
+    return data;
+}
+
+// fetchWeatherData('33.44', '-94.04');
+// fetchCoordinates('London');
