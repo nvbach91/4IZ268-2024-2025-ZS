28912c8e1e870eff4b7bbfdc950c5277ef705e10
BabkaM <michal.babka07@seznam.cz>
2024-12-15 21:20:58 +0100

    Update main.js

--- www/babm19/sp02/js/main.js
+++ www/babm19/sp02/js/main.js
@@ -5,3 +5,20 @@ const api = new TodoistApi("0123456789abcdef0123456789")
 api.addProject({ name: "To-do list" })
     .then((project) => console.log(project))
     .catch((error) => console.log(error))
+
+    [
+        {
+            id: "220474322",
+            parentId: null,
+            order: 0,
+            color: "grey",
+            name: "Inbox",
+            commentCount: 10,
+            isShared: false,
+            isFavorite: false,
+            isInboxProject: true,
+            isTeamInbox: false,
+            url: "https://todoist.com/showProject?id=220474322",
+            viewStyle: "list"
+        }
+    ]
