e0bb169f7d8149a11de0355d6d0e2a23e273e4ef
narcisusss <95647838+narcisusss@users.noreply.github.com>
2025-01-09 23:06:54 +0100

    Update tree.js
    new

--- www/leva06/sp2/js/tree.js
+++ www/leva06/sp2/js/tree.js
@@ -1,40 +1,38 @@
-// Tree.js: Represents the tree obstacle in the Kitten Jump Game
 export default class Tree {
-    constructor(ctx, x, y, width, height, imageSrc) {
-      this.ctx = ctx;
-      this.x = x;
-      this.y = y;
-      this.width = width;
-      this.height = height;
-  
-      this.image = new Image();
-      this.image.src = imageSrc; // Tree image source
-    }
-  
-    update(speed, deltaTime) {
-      // Move the tree to the left
-      this.x -= speed * deltaTime;
-  
-      // Reset the tree's position when it moves off-screen
-      if (this.x + this.width < 0) {
-        this.x = this.ctx.canvas.width;
-      }
-    }
-  
-    draw() {
-      // Draw the tree on the canvas
-      this.ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
-    }
-  
-    collideWith(player) {
-      // Precise collision detection: Player must be on the ground
-      return (
-        player.y >= player.yStandingPosition && // Ensure player is on the ground
-        player.x < this.x + this.width &&
-        player.x + player.width > this.x &&
-        player.y < this.y + this.height &&
-        player.y + player.height > this.y
-      );
+  constructor(ctx, x, y, width, height, image) {
+    this.ctx = ctx;
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+
+    this.image = new Image();
+    this.image.src = image; // Tree image
+  }
+
+  update(speed, deltaTime) {
+    this.x -= speed * deltaTime;
+
+    // If the tree goes off the screen, reset it with a larger gap
+    if (this.x + this.width < 0) {
+      this.x = this.ctx.canvas.width + Math.random() * 300 + 200; // Increased gap between trees
     }
   }
-  
+
+  draw() {
+    this.ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+  }
+
+  collideWith(player) {
+    return (
+      player.x < this.x + this.width &&
+      player.x + player.width > this.x &&
+      player.y < this.y + this.height &&
+      player.y + player.height > this.y
+    );
+  }
+
+  reset() {
+    this.x = this.startingX; // Resets the tree position
+  }
+}
