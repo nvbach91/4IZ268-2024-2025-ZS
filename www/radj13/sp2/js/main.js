f372ed573acdd8b010505f91d4ce3609b51c3100
HonzaRada <134331365+HonzaRada@users.noreply.github.com>
2025-01-24 03:28:45 +0100

    updated version
    changed or added all requested things

--- www/radj13/sp2/js/main.js
+++ www/radj13/sp2/js/main.js
@@ -7,13 +7,18 @@ const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/w500';
 document.getElementById('search-form').addEventListener('submit', async (event) => {
     event.preventDefault(); // Prevent the default form submission
     const searchValue = document.getElementById('search-input').value.trim();
-    if (!searchValue) {
-        showPopupMessage('You can\'t search with no input!', 'error'); // Show error if input is empty
+    const language = document.getElementById('language-filter').value;
+    const year = document.getElementById('year-filter').value;
+    const genre = document.getElementById('genre-filter').value;
+    console.log(language);
+    if (!searchValue && !language && !year && !genre) {
+        showPopupMessage('You must enter a search term or select at least one filter!', 'error'); // Show error if input is empty
         return;
     }
-    const data = await fetchMovies(searchValue);
+    const data = await fetchMovies(searchValue, language, year, genre);
     if (data.length === 0) {
-        displayMovies(data, 'search-results-list');
+        localStorage.setItem('searchResults', JSON.stringify(data));
+        showSearchResults();
         showPopupMessage('No movies found!', 'error'); // Show error if no movies are found
         return;
     }
@@ -22,6 +27,45 @@ document.getElementById('search-form').addEventListener('submit', async (event) 
     localStorage.setItem('currentView', 'searchResults');
     showSearchResults();
 });
+
+// Function to show the filters modal
+const showFiltersModal = () => {
+    const modal = document.getElementById('filters-modal');
+    modal.style.display = 'block';
+};
+
+// Function to hide the filters modal
+const hideFiltersModal = () => {
+    const modal = document.getElementById('filters-modal');
+    modal.style.display = 'none';
+};
+
+// Function to clear filter inputs
+const clearFilters = () => {
+    document.getElementById('language-filter').value = '';
+    document.getElementById('year-filter').value = '';
+    document.getElementById('genre-filter').value = '';
+};
+
+// Event listener for the clear filters button
+document.getElementById('clearFiltersButton').addEventListener('click', clearFilters);
+
+// Event listener for toggle filter button
+document.getElementById('filtersButton').addEventListener('click', showFiltersModal);
+
+// Event listener for the close button in the filters modal
+document.querySelector('.closeFiltersButton').addEventListener('click', hideFiltersModal);
+
+// Event listener to close the filters modal when clicking outside of it
+window.addEventListener('click', (event) => {
+    const modal = document.getElementById('filters-modal');
+    if (event.target == modal) {
+        modal.style.display = 'none';
+    }
+});
+
+// Event listener for the Apply Filters button
+document.getElementById('applyFiltersButton').addEventListener('click', hideFiltersModal);
 
 // Event listener for the Home button
 document.getElementById('homeButton').addEventListener('click', () => {
@@ -48,7 +92,7 @@ const libraryResultsList = document.getElementById('library-results-list');
 const homeSection = document.getElementById('home-section');
 const movieModal = document.getElementById('movie-modal');
 const movieDetailsContent = document.getElementById('movie-details-content');
-const closeButton = document.querySelector('.close-button');
+const closeButton = document.querySelector('.closeButton');
 
 // Event listener for closing the movie modal
 closeButton.addEventListener('click', () => {
@@ -79,13 +123,31 @@ window.addEventListener('load', () => {
     } else {
         displayHome(); // Default to displaying the home view
     }
+    populateGenreFilter();
+    populateLanguageFilter();
     hideLoadingSpinner(); // Hide the loading spinner
 });
 
 // Fetch movies based on search value
-const fetchMovies = async (searchValue) => {
+const fetchMovies = async (searchValue, language, year, genre) => {
     showLoadingSpinner(); // Show the loading spinner
-    const result = await fetch(`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${searchValue}`); // Fetch searched movies from the API
+    let url = '';
+    if(searchValue){
+        url = `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${searchValue}`;
+    }
+    else{
+        url = `${BASE_URL}/discover/movie?api_key=${API_KEY}`;
+    }
+    if(genre){
+        url += `&with_genres=${genre}`;
+    }
+    if(year){
+        url += `&primary_release_year=${year}`;
+    }
+    if(language){
+        url += `&language=${language}`;
+    }
+    const result = await fetch(url); // Fetch searched movies from the API
     const data = await result.json();
     hideLoadingSpinner(); // Hide the loading spinner
     return data.results;
@@ -109,6 +171,44 @@ const fetchNewestMovies = async () => {
     return data.results;
 };
 
+// Fetch available genres from the API
+const fetchGenres = async () => {
+const result = await fetch(`${BASE_URL}/genre/movie/list?api_key=${API_KEY}`);
+const data = await result.json();
+return data.genres;
+};
+
+// Fetch available languages from the API
+const fetchLanguages = async () => {
+    const result = await fetch(`${BASE_URL}/configuration/languages?api_key=${API_KEY}`);
+    const data = await result.json();
+    return data;
+};
+
+// Populate genre filter options
+const populateGenreFilter = async () => {
+    const genres = await fetchGenres();
+    const genreFilter = document.getElementById('genre-filter');
+    genres.forEach(genre => {
+        const option = document.createElement('option');
+        option.value = genre.id;
+        option.textContent = genre.name;
+        genreFilter.appendChild(option);
+    });
+};
+
+// Populate language filter options
+const populateLanguageFilter = async () => {
+    const languages = await fetchLanguages();
+    const languageFilter = document.getElementById('language-filter');
+    languages.forEach(language => {
+        const option = document.createElement('option');
+        option.value = language.iso_639_1;
+        option.textContent = language.english_name;
+        languageFilter.appendChild(option);
+    });
+};
+
 // Display movies in the specified container
 const displayMovies = (data, containerId) => {
     const container = document.getElementById(containerId); // Get the container element by ID
@@ -124,17 +224,26 @@ const displayMovies = (data, containerId) => {
         const posterPath = movie.poster_path ? `${IMAGE_BASE_URL}${movie.poster_path}` : null; // Get the poster path
         const isInLibrary = library.some(item => item.id === movie.id); // Check if the movie is in the library
         movieElement.innerHTML = `
-            ${posterPath ? `<img src="${posterPath}" alt="${movie.title}">` : `<div class="placeholder">No Image Available</div>`}
-            <h3>${movie.title}</h3>
-            <p>Rating: ${movie.vote_average}</p>
-            <div class="buttons">
-                <button class="details-button">More Details</button>
-                <button class="${isInLibrary ? 'remove-button' : 'add-button'}">${isInLibrary ? 'Remove from Library' : 'Add to Library'}</button>
+            <div class="movie-poster">
+                ${posterPath ? `<img src="${posterPath}" alt="${movie.title}">` : `<div class="placeholder">No Image Available</div>`}
+                <h3>${movie.title}</h3>
+                <p>Rating: ${movie.vote_average.toFixed(1)}/10</p>
+                <div class="buttons">
+                    <button class="details-button">More Details</button>
+                    <button class="${isInLibrary ? 'remove-button' : 'add-button'}">${isInLibrary ? 'Remove from Library' : 'Add to Library'}</button>
+                </div>
             </div>
         `;
-        movieElement.querySelector('.details-button').addEventListener('click', () => showMovieDetails(movie.id));
+        movieElement.querySelector('.movie-poster').addEventListener('click', () => showMovieDetails(movie.id));
+        movieElement.querySelector('.details-button').addEventListener('click', (event) => {
+            event.stopPropagation(); // Stop event propagation to prevent the movie details modal from opening
+            showMovieDetails(movie.id)
+        });
         const toggleButton = movieElement.querySelector(isInLibrary ? '.remove-button' : '.add-button'); // Get the toggle button
-        toggleButton.addEventListener('click', () => toggleLibrary(movie, toggleButton));
+        toggleButton.addEventListener('click', (event) => {
+            event.stopPropagation(); // Stop event propagation to prevent the movie details modal from opening
+            toggleLibrary(movie, toggleButton)}
+        );
         container.appendChild(movieElement);
     });
 };
@@ -149,7 +258,7 @@ const showMovieDetails = (movieId) => {
                 ${posterPath ? `<img src="${posterPath}" alt="${data.title}">` : `<div class="placeholder">No Image Available</div>`}
                 <p>${data.overview}</p>
                 <p>Release Date: ${data.release_date}</p>
-                <p>Rating: ${data.vote_average}</p>
+                <p>Rating: ${data.vote_average.toFixed(1)}/10</p>
                 <p>Runtime: ${data.runtime} minutes</p>
                 <p>Genres: ${data.genres.map(genre => genre.name).join(', ')}</p>
             `;
@@ -194,7 +303,7 @@ const removeFromLibrary = (movieId) => {
     library = library.filter(movie => movie.id !== movieId);
     localStorage.setItem('library', JSON.stringify(library)); // Update the library in local storage
     displayLibrary(); // Refresh the library view
-    showPopupMessage('Movie removed from library!', 'success');
+    showPopupMessage('Movie removed from library!', 'lib-remove');
 };
 
 // Toggle a movie in the library (add/remove)
@@ -206,7 +315,7 @@ const toggleLibrary = (movie, button) => {
         button.textContent = 'Add to Library';
         button.classList.remove('remove-button');
         button.classList.add('add-button');
-        showPopupMessage('Movie removed from library!', 'success');
+        showPopupMessage('Movie removed from library!', 'lib-remove');
     } else {
         library.push(movie);
         localStorage.setItem('library', JSON.stringify(library)); // Update the library in local storage
@@ -229,16 +338,22 @@ const displayLibrary = () => {
         movieElement.className = 'movie';
         const posterPath = movie.poster_path ? `${IMAGE_BASE_URL}${movie.poster_path}` : null; // Get the poster path
         movieElement.innerHTML = `
-            ${posterPath ? `<img src="${posterPath}" alt="${movie.title}">` : `<div class="placeholder">No Image Available</div>`}
-            <h3>${movie.title}</h3>
-            <p>Rating: ${movie.vote_average}</p>
-            <div class="buttons">
-                <button class="details-button">More Details</button>
-                <button class="remove-button">Remove from Library</button>
+            <div class="movie-poster">
+                ${posterPath ? `<img src="${posterPath}" alt="${movie.title}">` : `<div class="placeholder">No Image Available</div>`}
+                <h3>${movie.title}</h3>
+                <p>Rating: ${movie.vote_average.toFixed(1)}/10</p>
+                <div class="buttons">
+                    <button class="details-button">More Details</button>
+                    <button class="remove-button">Remove from Library</button>
+                </div>
             </div>
         `;
+        movieElement.querySelector('.movie-poster').addEventListener('click', () => showMovieDetails(movie.id));
         movieElement.querySelector('.details-button').addEventListener('click', () => showMovieDetails(movie.id));
-        movieElement.querySelector('.remove-button').addEventListener('click', () => removeFromLibrary(movie.id));
+        movieElement.querySelector('.remove-button').addEventListener('click', (event) => {
+            event.stopPropagation(); // Stop event propagation to prevent the movie details modal from opening
+            removeFromLibrary(movie.id)
+        });
         libraryResultsList.appendChild(movieElement);
     });
 };
@@ -267,6 +382,21 @@ const displayHome = async () => {
     const newestMovies = await fetchNewestMovies();
     displayMovies(newestMovies, 'newest-list');
 };
+
+// Event listener for tab buttons
+document.querySelectorAll('.tab-button').forEach(button => {
+    button.addEventListener('click', () => {
+        const tab = button.getAttribute('data-tab');
+
+        // Remove active class from all tab buttons and contents
+        document.querySelectorAll('.tab-button').forEach(btn => btn.classList.remove('active'));
+        document.querySelectorAll('.tab-content').forEach(content => content.classList.remove('active'));
+
+        // Add active class to the clicked tab button and corresponding content
+        button.classList.add('active');
+        document.getElementById(tab).classList.add('active');
+    });
+});
 
 // Function to show the loading spinner
 const showLoadingSpinner = () => {
